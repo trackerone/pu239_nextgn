b87470e92f30402f28791c53507b085d4be7b526
trackerone <51250404+trackerone@users.noreply.github.com>
2025-08-15 06:14:52 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,16 +1,19 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        // api: __DIR__.'/../routes/api.php', // uncomment når/ hvis du får en api.php
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
     )
-    ->withMiddleware(function ($middleware) {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function ($exceptions) {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->create();
+    ->create(); // <<< VIGTIGT
