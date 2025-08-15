f7768e87e1d482d8d6c25c0f281fac2946455598
trackerone <51250404+trackerone@users.noreply.github.com>
2025-08-15 06:03:38 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,21 @@
 <?php
 
-define('LARAVEL_START', microtime(true));
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
 require __DIR__.'/../vendor/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->handleRequest(
-    $app->make(Illuminate\Contracts\Http\Kernel::class)
-)->send();
+Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        // Tilføj evt. middleware her
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })
+    ->run();
