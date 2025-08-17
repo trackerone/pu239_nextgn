99c9b6025d52983d652ff7bdd8da09a8ab7ff00c
trackerone <51250404+trackerone@users.noreply.github.com>
2025-08-17 10:01:16 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,25 +3,21 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\View\ViewServiceProvider;
 
 return Application::configure(basePath: dirname(__DIR__))
-    // Registrér View-service (giver container-bindingen "view")
-    ->withProviders([
-        ViewServiceProvider::class,
-    ])
-    // Tænd facader (så Facade::... virker, inkl. View::...)
-    ->withFacades()
-    // Routing (tilpas stierne hvis du ikke har api/console)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // evt. global middleware
+        // Her kan du evt. tilføje/alias’e middleware.
+        // Eksempel (hvis du bruger fruitcake/php-cors):
+        // $middleware->alias(['cors' => \Fruitcake\Cors\HandleCors::class]);
+        // $middleware->append(\Fruitcake\Cors\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        // evt. exception-håndtering
+        //
     })
     ->create();
