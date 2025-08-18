3b516b4541a7e064cb92a0231a8e0fe723760cf5
trackerone <51250404+trackerone@users.noreply.github.com>
2025-08-18 20:22:21 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,18 +3,27 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Contracts\Container\BindingResolutionException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // optional
+        // tilføj evt. middleware her
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        // Lad standard handleren køre. Ingen response() her.
+        // Hvis containeren ikke kan binde en klasse (fx "view"),
+        // så svar med enkel tekst, ikke Blade-view:
+        $exceptions->render(function (BindingResolutionException $e, $request) {
+            return new \Illuminate\Http\Response(
+                'ERR: ' . $e->getMessage(),
+                500,
+                ['Content-Type' => 'text/plain']
+            );
+        });
     })
     ->create();
