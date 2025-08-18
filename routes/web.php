a5968db468ca4a322a45167e3f1b526c2eca2c6d
trackerone <51250404+trackerone@users.noreply.github.com>
2025-08-18 06:00:14 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,1 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome'); // kræver resources/views/welcome.blade.php
-});
+Route::get('/', fn () => 'OK');
