376a101c3f72f69593721c1bc4a0927693168c0d
trackerone <51250404+trackerone@users.noreply.github.com>
2025-08-19 06:12:44 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,7 @@
-welcome to pu239 nextgn
+<!doctype html>
+<html>
+  <head><meta charset="utf-8"><title>welcome to pu239 nextgn</title></head>
+  <body>
+    <h1>Laravel kører ✅</h1>
+  </body>
+</html>
