8132c9f89491cf84c8c4b18a47972efb890a13ed
trackerone <51250404+trackerone@users.noreply.github.com>
2025-08-19 05:52:31 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,19 +2,12 @@
 
 declare(strict_types=1);
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+define('LARAVEL_START', microtime(true));
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+$app = require __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
+$app->handleRequest(
+    $request = Illuminate\Http\Request::capture()
 );
-
-$response->send();
-
-$kernel->terminate($request, $response);
